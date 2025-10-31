9e1516f287069dcfad2e2e112b1f3e45164c554f
AnuXi313 <anuxi313@gmail.com>
2025-10-31 18:29:59 +0330

    Refactor App component to consolidate routing logic and enhance state management; replace custom Router hook with inline routing functionality; improve document title updates based on current path; streamline component rendering with dynamic path handling; remove unused Router file for cleaner codebase.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,92 @@
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import routes from "./constants/routes";
 import Header from "./layouts/Header";
 import Main from "./layouts/Main";
 import Footer from "./layouts/Footer";
-import Loader from "./components/Loader";
-import { ToastContainer } from "react-toastify";
 import MusicPlayer from "./layouts/MusicPlayer";
-import Router from "./hooks/useRouter";
+import Loader from "./components/Loader";
+import Hero from "./pages/Hero";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
-  const RouteComponent = Router();
+  const appName = "Web Tools"; // app name
+  const [currentPath, setCurrentPath] = useState(location.pathname);
+
+  // find jsx component by path
+  const getComponent = (path) => {
+    const route = routes.find((r) => r.path === path);
+
+    if (path === "/" || path === "/home") {
+      return <Hero />;
+    } else if (route && route.component) {
+      const Component = route.component;
+      return <Component />;
+    } else {
+      return <NotFound />;
+    }
+  };
+
+  useEffect(() => {
+    const route = routes.find((r) => r.path === currentPath);
+    document.title = route
+      ? `${appName} | ${route.name}`
+      : `${appName} : Not Found 404`;
+  }, [currentPath]);
+
+  // handle click on link by data-route
+  useEffect(() => {
+    const handleLinkClick = (e) => {
+      const target = e.target.closest("[data-route]");
+      if (!target) return;
+
+      e.preventDefault();
+      const newPath = target.dataset.route;
+
+      // save current path scroll position
+      history.scrollRestoration = "auto";
+
+      // prevent push same state just replace the path to history
+      if (history.state?.name === newPath) return;
+
+      if (history.state?.isModal) {
+        history.replaceState({ name: newPath }, "", newPath);
+      } else {
+        history.pushState({ name: newPath }, "", newPath);
+      }
+
+      setCurrentPath(newPath);
+
+      window.dispatchEvent(
+        new CustomEvent("route-change", { detail: newPath }),
+      );
+    };
+
+    document.addEventListener("click", handleLinkClick);
+    return () => {
+      document.removeEventListener("click", handleLinkClick);
+    };
+  }, []);
+
+  // handle (back / forward) button on browser
+  useEffect(() => {
+    const handlePopState = () => {
+      const path = location.pathname;
+      setCurrentPath(path);
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
 
   return (
     <div role="application" className="overflow-hidden">
       <Header />
       <Suspense fallback={<Loader />}>
-        <Main>{RouteComponent}</Main>
+        <Main>{getComponent(currentPath)}</Main>
       </Suspense>
       <Footer />
       <MusicPlayer />
